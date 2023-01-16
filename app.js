b3bf30b5691386ac7b45f1c1a355f0f9a7c127b1
Jero Jimenez <jerodepiran@gmail.com>
2023-01-16 17:10:18 -0300

    upload project

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const readExcelFile = require("./utils/readExcelFile");
+var cors = require('cors')
+
+const app = express();
+app.use(cors())
+
+// body parser to json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// API ROUTES
+
+// app.post("/postFile",(req,res)=>{
+//     const data=req.body;
+// });
+
+// ["code", "description", "brand", "iva", "price"]
+
+app.get("/api/productos/taladro",(req,res)=>{
+    const products = readExcelFile(4, [0, 1, -1, 6, 7], "./excel-files/taladro.xlsx");
+
+	
+        res.send(products.map(product =>
+        ({...product, price: product.price}))); 
+		
+});
+
+app.get("/api/productos/trebol",(req,res)=>{
+    const products = readExcelFile(3, [0, 1, -1, 3, 2], "./excel-files/trebol.xlsx");
+    // set iva price
+    res.send(products.map(product =>
+        product.iva == "B" ?
+            {
+                ...product,
+                price:  product.price * 1.1 * 1.5
+            } :
+            {
+                ...product,
+                price: product.price * 1.21 * 1.5
+            } ));
+});
+
+app.get("/api/productos/cerrajeria",(req,res)=>{
+    const products = readExcelFile(6, [3, 1, 0, -1, 2], "./excel-files/cerrajeria.xlsx");
+    res.send(products.map(product =>
+        ({...product, price: product.price *1.11 * 1.5})));
+});
+app.get("/api/productos/paulo",(req,res)=>{
+    const products = readExcelFile(1, [0, 1, -1, -1, 2], "./excel-files/paulo1.xlsx");
+    res.send(products.map(product =>
+        ({...product, price: product.price *1.11 * 1.5})));
+});
+app.get("/api/productos/nexo",(req,res)=>{
+    const products = readExcelFile(23, [0, 2, -1, -1, 9], "./excel-files/nexo.xlsx");
+    res.send(products.map(product =>
+        ({...product, price: product.price* 1.21* 1.5})));
+});
+app.get("/api/productos/fg",(req,res)=>{
+    const products = readExcelFile(5, [0, 1, 2, -1, 3], "./excel-files/fg.xlsx");
+    res.send(products.map(product =>
+        ({...product, price: (parseFloat(product.price) - parseFloat(product.price)*0.2) * 1.50})));  
+});
+
+app.get("/api/productos/bethular",(req,res)=>{
+    const products = readExcelFile(6, [1, 2, -1, -1, 3], "./excel-files/bethular.xlsx");
+    res.send(products.map(product =>
+        ({...product, price: product.price* 1.21* 1.5})));
+});
+
+app.get("/api/productos/jm",(req,res)=>{
+    const products = readExcelFile(2, [0, 1, 2, -1, 4], "./excel-files/jm.xlsx");
+    res.send(products.map(product =>
+        ({...product, price: product.price * 1.5})));
+
+});
+app.get("/api/productos/paulo",(req,res)=>{
+    const products = readExcelFile(1, [0, 1, -1, -1, 2], "./excel-files/paulo.xlsx");
+    res.send(products.map(product =>
+        ({...product, price: product.price * 1.5}))); 
+});
+app.get("/api/productos/foxs",(req,res)=>{
+    const products = readExcelFile(2, [0, 1, -1, -1, 2], "./excel-files/foxs.xlsx");
+    res.send(products.map(product =>
+        ({...product, price: (parseFloat(product.price) - parseFloat(product.price)*0.2) * 1.50}))); 
+});
+app.get("/api/productos/ciardi",(req,res)=>{
+    const products = readExcelFile(12, [3, 4, 5, 6, 7], "./excel-files/ciardi.xlsx");
+  	res.send(products.map(product =>
+        product.iva == "10.5" ?
+            {
+                ...product,
+                price:  product.price * 1.1 * 1.5
+            } :
+            {
+                ...product,
+                price: product.price * 1.21 * 1.5
+            } ));
+});
+// start server
+app.listen(3002,()=>{
+    console.log("Listening in port 3001");
+});
---
+++ utils/readExcelFile.js
@@ -0,0 +1,42 @@
+
+const xslx = require('xlsx'); 
+ 
+ module.exports = function readExcelFile (initRows, columns, filePath) {
+    try {
+        let obj = []; 
+        const workbook = xslx.readFile("" + filePath);
+        
+        const sheet = workbook.Sheets[workbook.SheetNames[0]];
+        const products = [];
+        let product = {};
+        let i=0;
+        var range = xslx.utils.decode_range(sheet['!ref']);
+        // iterate over rows
+        for (let rowNum = initRows; rowNum <= range.e.r; rowNum++) { 
+            // get cell[r,c] value where r = row, c = column 
+            const codeCell = sheet[xslx.utils.encode_cell({r: rowNum, c: columns[0]})];  
+            const descriptionCell = sheet[xslx.utils.encode_cell({r: rowNum, c: columns[1]})];
+            const brandCell =  sheet[xslx.utils.encode_cell({r: rowNum, c: columns[2] != -1 ? columns[2] : columns[0]})];
+            const ivaCell = sheet[xslx.utils.encode_cell({r: rowNum, c: columns[3] != -1 ? columns[3] : columns[0]})]; 
+            const priceCell = sheet[xslx.utils.encode_cell({r: rowNum, c: columns[4]})]; 
+            // create new product
+            if (codeCell) {
+                product = {
+                    id: (filePath.split("/")[2][0] + i++),
+                    cod : codeCell?.v,
+                    description: descriptionCell?.v,
+                    brand: columns[2] != -1 ? brandCell?.v : "", 
+                    iva: columns[3] != -1 ? ivaCell?.v : "",
+                    price: priceCell?.v,
+                    amount:0
+                }
+                // add product to list
+                products.push(product); 
+            }
+        }
+         return products;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
