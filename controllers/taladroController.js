352fbb5309abc9acb47c22e3fb20271ec062f671
Jero Jimenez <jerodepiran@gmail.com>
2023-09-25 16:51:48 -0300

    change lists

--- controllers/taladroController.js
+++ controllers/taladroController.js
@@ -4,17 +4,25 @@ module.exports.getProducts = (req, res) => {
   const products = readExcelFile(
     1,
     4,
-    [0, 1, -1, 6, 7],
+    [0, 1, 6, 5, 3],
     "./excel-files/taladro.xlsx"
   );
 
-  res.send(products.map((product) => ({ ...product, price: product.price })));
+  res.send(products.map((product) => {
+    (product.iva==="A")?
+      product.price= product.price*1.21 :
+      (product.iva==="C")?
+        product.price=product.price*1.105:
+        product.price=product.price
+        return{ ...product,price:product.price*1.5}
+
+     }))
 };
 module.exports.getProduct = (req, res) => {
   const products = readExcelFile(
     1,
     4,
-    [0, 1, -1, 6, 7],
+    [0, 1, 6, 5, 3],
     "./excel-files/taladro.xlsx"
   );
 
