4ea8501b0be3f4961c8e63eb18ba0bf65456c743
Jero Jimenez <jerodepiran@gmail.com>
2024-04-09 16:05:20 -0300

    change lists

--- controllers/cerrajeriaController.js
+++ controllers/cerrajeriaController.js
@@ -4,7 +4,7 @@ module.exports.getProducts = (req, res) => {
   const products = readExcelFile(
     1,
     6,
-    [3, 1, 0, -1, 2],
+    [1, 2, 0, -1, 3],
     "./excel-files/cerrajeria.xlsx"
   );
   res.send(
